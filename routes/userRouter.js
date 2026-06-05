93195b24aab4e051146cd0c64f62f1ab4d337a61
jjwon0407 <jjwon0407@naver.com>
2026-06-05 22:18:19 +0900

    [Fix] jwt 인증 삭제

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,12 +5,12 @@ const userController = require('../controllers/userController');
 const { authenticateJWT } = require('../middleware/authMiddleware');
 
 // 유저 정보 조회
-router.get('/:userId', authenticateJWT, userController.getUserInfo);
+router.get('/:userId', userController.getUserInfo);
 
 // 유저 레벨 업데이트
-router.patch('/:userId/level', authenticateJWT, userController.updateUserLevel);
+router.patch('/:userId/level', userController.updateUserLevel);
 
 // 유저 닉네임 bulk 조회
-router.post('/nicknames', authenticateJWT, userController.getUserNicknames);
+router.post('/nicknames', userController.getUserNicknames);
 
 module.exports = router;
